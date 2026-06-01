9bdedbf4c83f7abfe3a2f97425f383cad3ee7079
Engelbert Jara <engelbert.jara@gmail.com>
2026-05-31 21:37:11 -0400

    Agregando ejercicios basicos de JS

---
+++ ejercicios-1.js
@@ -0,0 +1,73 @@
+// 1.Crea dos variables: una con tu nombre y otra con tu apellido
+
+let nombre ="Engelbert";
+let apellido = "Jara";
+console.log (nombre, apellido);
+
+// 2. Crea una variable con un número. Súmale 5, luego multiplícalo por 3 e imprime el resultado.
+
+let numero = 10;
+numero = (numero + 5) * 3;
+console.log (numero);
+
+// 3. Crea una variable con tu comida favorita e imprímela en consola.
+let comida_favorita = "Zapallo italiano";
+console.log (comida_favorita);
+
+// 4. Crea dos variables a = 10 y b = 3
+
+let a = 10;
+let b = 3;
+console.log (a + b);
+console.log (a - b);
+console.log (a * b);
+console.log (a % b);
+
+// 4.1 Ahora cambia los valores por tipo de datos strings y luego string y number ¿Qué sucede en cada uno?
+
+let x = "10";
+let y = 3;
+console.log (x + y);
+console.log (x - y);
+console.log (x * y);
+console.log (x % y);
+
+// 5. Crea una variable
+
+let edad = 15;
+console.log (edad>10);
+console.log (edad<18);
+
+// 6. Crea una variable
+
+nomerovariable = 8;
+console.log (nomerovariable >5 && nomerovariable <10);
+
+// 7. Crea una variable
+
+let nota = 6;
+console.log (nota == 7);
+
+// 8. Crea una variable temperatura
+
+let temperatura = 25;
+console.log (temperatura >= 30);
+
+// 9. Crea dos números
+
+let numero1 = 7;
+let numero2 = 5;
+console.log (numero1 == numero2);
+console.log (numero1 > numero2);
+
+// 10. Crea una variable con tu edad
+
+let edad1=17;
+let tienePermiso=true;
+console.log("puede conducir", edad1 >= 18 || tienePermiso);
+
+// 11. Crea usuario y contraseña
+
+let Usuario = "Admin";
+let Pasword = "1234";
+console.log("usuario es", Usuario, "y", "contraseña es", Pasword);
